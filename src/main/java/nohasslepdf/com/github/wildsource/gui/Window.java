406f1e34a70af2799a722b8afe20e85c1a811dee
WildSource <ilovetrap123@hotmail.com>
2023-10-11 11:03:26 -0400

    add filechooser

--- src/main/java/nohasslepdf/com/github/wildsource/gui/Window.java
+++ src/main/java/nohasslepdf/com/github/wildsource/gui/Window.java
@@ -1,13 +1,21 @@
 package nohasslepdf.com.github.wildsource.gui;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import net.miginfocom.swing.MigLayout;
 
 public class Window {
 	private JFrame frame;
+	private JFileChooser chooser;
 
 	public Window() {
+		framesetup();
+		fileChooserSetup();
+	}
+
+	private void framesetup() {
 		this.frame = new JFrame();
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,6 +24,12 @@ public class Window {
 		frame.setLayout(new MigLayout());
 		frame.setTitle("AC4J");
 		frame.setVisible(true);
+	}
+
+	private void fileChooserSetup() {
+		chooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & PNG Images", "jpg", "png");
+		chooser.setFileFilter(filter);
 	}
 
 	public JFrame getFrame() {
